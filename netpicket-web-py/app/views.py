4da0a1d47ee9ad04db6eba00ec9f0f8a7bb83a89
7flying <diez.f.irene@gmail.com>
2015-10-11 01:07:05 +0200

    Backup google oauth2 torture

--- netpicket-web-py/app/views.py
+++ netpicket-web-py/app/views.py
@@ -1,12 +1,47 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+import os
+
+from requests_oauthlib import OAuth2Session
+from flask import render_template, request, redirect, url_for, session
+from flask.json import jsonify
 
 from app import app
 
 
+client_id = '25024647321-ch635ssha0lmo73lr24688slcb4mak4j.apps.googleusercontent.com'
+client_secret = ''
+authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
+token_url = "https://accounts.google.com/o/oauth2/token"
+scope = ["https://www.googleapis.com/auth/userinfo.email",
+         "https://www.googleapis.com/auth/userinfo.profile"]
+redirect_uri = 'http://localhost:5000/callback'
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    """ """
-    return render_template('index.html')
+    google = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
+    authorization_url, state = google.authorization_url(authorization_base_url,
+                                                        access_type='offline',
+                                                        approval_prompt='force')
+
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+@app.route('/callback', methods=['GET', 'POST'])
+def callback():
+    google = OAuth2Session(client_id, state=session['oauth_state'])
+    print request.args
+    print request.__dict__
+    print request.url
+    print request.environ
+    token = google.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.environ['QUERY_STRING'])#,
+                               #code=request.args['code'])
+    session['oauth_token'] = token
+    r = google.get('https://www.googleapis.com/oauth2/v1/userinfo')
+    print r.content
+    return redirect(url_for('profile'))
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    return render_template('ok.html')
--- netpicket-web-py/config.py
+++ netpicket-web-py/config.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import uuid
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = str(uuid.uuid4())
+
