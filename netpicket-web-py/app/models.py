fe1252c384e521b5f7338196f3df35286add3300
7flying <diez.f.irene@gmail.com>
2015-12-03 16:51:10 +0100

    Add network management on db modify events
    -The events need to reference in which network did they occur.

--- netpicket-web-py/app/models.py
+++ netpicket-web-py/app/models.py
@@ -35,28 +35,69 @@ class User(db.Model):
         """False, as anonymous users aren't supported."""
         return False
 
-# ---- Keys ---- #
-USER = 'user:'
 
-F_NETWORK = 'network'
+# --- Network/buoy --- #
+# Network/buoys are stored in hashes, which hold the network's main properties.
+# The net id must be generated with _get_key_net()
+# When a new network is created add it to the user's list of networks.
+_KEY_NET_ID = 'net-id-auto:'
+_KEY_NET = 'net:{0}'
+_ATTR_NET_NAME = 'name'
+_ATTR_NET_IFACE = 'interface'
+_ATTR_NET_HADDR = 'hard-address'
+_ATTR_NET_SPEED = 'speed'
+_ATTR_NET_SEC  = 'security'
+_ATTR_NET_ADDR = 'address'
+_ATTR_NET_MASK = 'subnet-mask'
+_ATTR_NET_DEFR = 'default-route'
+_ATTR_NET_DNS1 = 'dns-1'
+_ATTR_NET_DNS2 = 'dns-2'
 
-# --- User-Networks --- #
+_KEY_NETS_USER = 'nets-user:user:{0}'
 
-def set_network(user, nname):
+def _get_key_net():
+    """Returns an str with the next network key id."""
+    return str(red.incr(_KEY_NET_ID))
+
+def get_user_networks(user_id):
+    """Returns all the user's networks."""
+    keys = red.lrange(_KEY_NETS_USER.format(user_id), 0, -1)
+    nets = []
+    for key in keys:
+        tmp = get_network(key)
+        if tmp is not None:
+            nets.append(tmp)
+    return nets
+
+def set_network(user, name, iface, haddress, speed, sec, address, submask,
+                defroute, dns1, dns2=None):
+    """Saves a network."""
+    key = _get_key_net()
+    pip = red.pipeline()
+    pip.rpush(_KEY_NETS_USER.format(user), key)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_NAME, name)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_IFACE, iface)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_HADDR, haddress)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_SPEED, speed)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_SEC, sec)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_ADDR, address)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_MASK, submask)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_DEFR, defroute)
+    pip.hset(_KEY_NET.format(key), _ATTR_NET_DNS1, dns1)
+    if dns2 != None:
+        pip.hset(_KEY_NET.format(key), _ATTR_NET_DNS2, dns2)
+    pip.execute()
+
+def get_network(network):
+    """Retrieves a network"""
+    return red.hgetall(_KEY_NET.format(key))
+
+def delete_network(user_id, net_id):
     # something
-    _l_set_network(user, nname)
-
-def delete_network(user, nname):
-    # something
-    _l_del_network(user, nname)
-
-# -- List of all Networks a user has -- #
-
-def _l_set_network(user, nname):
-    pass
-
-def _l_del_network(user, nname):
-    pass
+    pip = red.pipeline()
+    pip.delete(_KEY_NET.format(net_id))
+    pip.lrem(_KEY_NETS_USER.format(user_id), 1, net_id)
+    pip.execute()
 
 # --- Events --- #
 # Events are stored in hashes, which hold the event's main properties.
@@ -70,13 +111,13 @@ _ATTR_EVENT_DATE = 'date' #20151126
 _ATTR_EVENT_DAY = 'day' # Wed 5 Oct
 _ATTR_EVENT_TIME = 'time' #6:12
 _ATTR_EVENT_PRIO = 'priority'
+_ATTR_EVENT_NETID = 'netid'
 
 _KEY_EVENTS_USER_DAY = 'event-ids:user:{0}:day:{1}'
 
 def _get_key_event():
     """Returns an str with the next alert key id."""
-    ret = red.incr(_KEY_EVENT_ID)
-    return str(ret)
+    return str(red.incr(_KEY_EVENT_ID))
 
 def save_event(user_id, event_desc, date, day, time, priority):
     """Saves an alert."""
@@ -85,7 +126,7 @@ def save_event(user_id, event_desc, date, day, time, priority):
     pipe.lpush(_KEY_EVENTS_USER_DAY.format(user_id, date), key)
     pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_DESC, event_desc)
     pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_DATE, date)
-    pipe.hset( _KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_DATE, date)
+    pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_DATE, date)
     pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_DAY, day)
     pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_TIME, time)
     pipe.hset(_KEY_EVENT_USER.format(key, user_id), _ATTR_EVENT_PRIO, priority)
