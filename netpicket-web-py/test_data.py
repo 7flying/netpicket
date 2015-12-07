c21ce421e050d3d097dba570ca09dfba3f162368
7flying <diez.f.irene@gmail.com>
2015-12-07 13:24:15 +0100

    More mockup data

--- netpicket-web-py/test_data.py
+++ netpicket-web-py/test_data.py
@@ -3,11 +3,11 @@
 Generates testing data.
 """
 
-import random, datetime, itertools
+import random, datetime, itertools, string
 import app.models as models
 import app.const as const
 
-def generate_mockup_data(networks=2, days=3, events_day=10):
+def generate_mockup_data(networks=2, days=3, events_day=10, entries=5):
     """Generate mockup data."""
     users = models.User.query.all()
     for user in users:
@@ -25,12 +25,20 @@ def generate_mockup_data(networks=2, days=3, events_day=10):
                 then = now - datetime.timedelta(days=day)
                 for _ in itertools.repeat(None, events_day):
                     for network in models.get_user_networks(user.id):
-                        models.save_event(user.id, network, 'Event ' + str(random.randint(1, 100)),
+                        models.save_event(user.id, network['id'],
+                                          'Event ' + str(random.randint(1, 100)),
                                           then.strftime(const.STRTIME_DATE),
                                           then.strftime(const.STRTIME_DAY),
                                           then.strftime(const.STRTIME_TIME),
                                           const.PRIORITY_COLOUR[
                                               random.randint(0, 3)])
+        netids = [net['id'] for net in models.get_user_networks(user.id)]
+        for _ in itertools.repeat(None, entries):
+            models.save_entry(str(user.id), random.choice(['B', 'W']),
+                              ''.join(random.choice(string.letters) for _ in range(15)),
+                              'FC:AA:25:32:FC:' + str(random.randint(10, 32)),
+                              '192.168.1.' + str(random.randint(1, 254)),
+                              [random.choice(netids)])
 
 if __name__ == '__main__':
     generate_mockup_data()
